9199bd49477e2bc820b000186eaf560c022226ef
SandLotLab <bighomieced@proton.me>
2026-02-20 10:35:36 -0800

    Update middleware.js

--- functions/middleware.js
+++ functions/middleware.js
@@ -1,10 +1,10 @@
 export async function onRequest({ request, next }) {
-  const url = new URL(request.url);
   const host = request.headers.get("host") || "";
+  const url = new URL(request.url);
 
+  // If someone uses the default Pages domain, kick them to the .app domain.
   if (host.endsWith(".pages.dev")) {
     url.host = "surewhynot.app";
-    url.protocol = "https:";
     return Response.redirect(url.toString(), 301);
   }
 
